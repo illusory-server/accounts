0c229949c288bdb5a5629b12e6390deec8b4221e
Karimov Marlen <mmkarimov>
2024-11-17 04:23:50 +0300

    refactor repository to cqrs

--- internal/domain/repository/account.go
+++ internal/domain/repository/account.go
@@ -2,31 +2,33 @@ package repository
 
 import (
 	"context"
-	"github.com/illusory-server/accounts/internal/domain/model"
+	"github.com/illusory-server/accounts/internal/domain/entity"
 	"github.com/illusory-server/accounts/internal/domain/query"
 	"github.com/illusory-server/accounts/internal/domain/vo"
 )
 
-type Accounts interface {
-	Create(ctx context.Context, account *model.Account) (*model.Account, error)
+type AccountCommand interface {
+	Create(ctx context.Context, account *entity.Account) (*entity.Account, error)
 
-	HasById(ctx context.Context, id vo.ID) (bool, error)
-	HasByEmail(ctx context.Context, email string) (bool, error)
-	HasByNickname(ctx context.Context, nickname string) (bool, error)
-
-	GetById(ctx context.Context, id vo.ID) (*model.Account, error)
-	GetByEmail(ctx context.Context, email string) (*model.Account, error)
-	GetByNickname(ctx context.Context, nickname string) (*model.Account, error)
-	GetByQuery(ctx context.Context, query *query.Pagination) ([]*model.Account, error)
-	GetPageCountByLimit(ctx context.Context, limit uint64) (uint64, error)
-
-	DeleteById(ctx context.Context, id vo.ID) error
+	DeleteById(ctx context.Context, id string) error
 	DeleteByEmail(ctx context.Context, email string) error
 	DeleteByNickname(ctx context.Context, nickname string) error
 
-	UpdateById(ctx context.Context, account *model.Account) (*model.Account, error)
-	UpdatePasswordById(ctx context.Context, id vo.ID, newPassword string) (*model.Account, error)
-	UpdateRoleById(ctx context.Context, id vo.ID, role string) (*model.Account, error)
+	UpdateById(ctx context.Context, account *entity.Account) (*entity.Account, error)
+	UpdatePasswordById(ctx context.Context, id string, newPassword string) (*entity.Account, error)
+	UpdateRoleById(ctx context.Context, id string, role string) (*entity.Account, error)
+}
 
-	CheckAccountRoleById(ctx context.Context, id vo.ID, expectedRole string) (bool, error)
+type AccountQuery interface {
+	HasById(ctx context.Context, id string) (bool, error)
+	HasByEmail(ctx context.Context, email string) (bool, error)
+	HasByNickname(ctx context.Context, nickname string) (bool, error)
+
+	GetById(ctx context.Context, id string) (*entity.Account, error)
+	GetByEmail(ctx context.Context, email string) (*entity.Account, error)
+	GetByNickname(ctx context.Context, nickname string) (*entity.Account, error)
+	GetByQuery(ctx context.Context, query *query.Pagination) ([]*entity.Account, error)
+	GetPageCountByLimit(ctx context.Context, limit uint64) (uint64, error)
+
+	CheckAccountRoleById(ctx context.Context, id string, expectedRole vo.AccountRoleType) (bool, error)
 }
