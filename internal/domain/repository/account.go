8a0e1da0bef134e01e92634118af35865e0be99f
Karimov Marlen <mmkarimov>
2025-01-18 23:07:31 +0300

    869780uvu - refactor repository interface

--- internal/domain/repository/account.go
+++ internal/domain/repository/account.go
@@ -8,14 +8,17 @@ import (
 
 type AccountCommand interface {
 	Create(ctx context.Context, account *entity.Account) (*entity.Account, error)
+	CreateMany(ctx context.Context, accounts []*entity.Account) error
+
+	UpdateInfoById(ctx context.Context, id vo.ID, info vo.AccountInfo) error
+	UpdateNicknameById(ctx context.Context, id vo.ID, nickname string) error
+	UpdateAvatarLinkById(ctx context.Context, id vo.ID, link vo.Link) error
+	UpdatePasswordById(ctx context.Context, id vo.ID, newPassword vo.Password) error
+	UpdateRoleById(ctx context.Context, id vo.ID, role vo.Role) error
 
 	DeleteById(ctx context.Context, id string) error
 	DeleteByEmail(ctx context.Context, email string) error
 	DeleteByNickname(ctx context.Context, nickname string) error
-
-	UpdateById(ctx context.Context, account *entity.Account) (*entity.Account, error)
-	UpdatePasswordById(ctx context.Context, id string, newPassword string) (*entity.Account, error)
-	UpdateRoleById(ctx context.Context, id string, role string) (*entity.Account, error)
 }
 
 type AccountQuery interface {
@@ -24,9 +27,11 @@ type AccountQuery interface {
 	HasByNickname(ctx context.Context, nickname string) (bool, error)
 
 	GetById(ctx context.Context, id string) (*entity.Account, error)
+	GetByIds(ctx context.Context, ids []string) ([]*entity.Account, error)
 	GetByEmail(ctx context.Context, email string) (*entity.Account, error)
 	GetByNickname(ctx context.Context, nickname string) (*entity.Account, error)
 	GetByQuery(ctx context.Context, query vo.Query) ([]*entity.Account, error)
+
 	GetPageCountByLimit(ctx context.Context, limit uint64) (uint64, error)
 
 	CheckAccountRoleById(ctx context.Context, id string, expectedRole vo.AccountRoleType) (bool, error)
